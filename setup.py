f35664128125756c2d5c221a796b1e4af17e805c
Craig Hobbs <craigahobbs@gmail.com>
2022-05-12 12:54:47 -0700

    markdown-up 1.4.4

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def main():
         description='Markdown viewer application',
         long_description=long_description,
         long_description_content_type='text/markdown',
-        version='1.4.3',
+        version='1.4.4',
         author='Craig A. Hobbs',
         author_email='craigahobbs@gmail.com',
         keywords='Markdown viewer application',
