f9fdbae24ee1e411fecc9c9d88d513c25ee92402
Craig Hobbs <craigahobbs@gmail.com>
2023-11-21 12:45:13 -0800

    use documented gunicorn standalone application

--- src/markdown_up/main.py
+++ src/markdown_up/main.py
@@ -32,8 +32,9 @@ def main(argv=None):
     args = parser.parse_args(args=argv)
 
     # Verify the path exists
-    is_file = args.path.endswith('.md')
-    if (is_file and not os.path.isfile(args.path)) or (not is_file and not os.path.isdir(args.path)):
+    is_dir = os.path.isdir(args.path)
+    is_file = not is_dir and os.path.isfile(args.path)
+    if not is_file and not is_dir:
         parser.exit(message=f'"{args.path}" does not exist!\n', status=2)
 
     # Determine the root
@@ -48,46 +49,45 @@ def main(argv=None):
 
     # If opening in the browser, define the when_ready function
     host = '127.0.0.1'
-    if args.no_browser:
-        when_ready = None
-    else:
-        def when_ready(_):
-            # Construct the URL
-            if is_file:
-                hash_args = encode_query_string({'url': os.path.basename(args.path)})
-                url = f'http://{host}:{args.port}/#{hash_args}'
-            else:
-                url = f'http://{host}:{args.port}/'
+    def when_ready(_):
+        # Do nothing?
+        if args.no_browser:
+            return
 
-            # Launch the web browser on a thread as webbrowser.open may block
-            webbrowser_thread = threading.Thread(target=webbrowser.open, args=(url,))
-            webbrowser_thread.daemon = True
-            webbrowser_thread.start()
+        # Construct the URL
+        if is_file:
+            hash_args = encode_query_string({'url': os.path.basename(args.path)})
+            url = f'http://{host}:{args.port}/#{hash_args}'
+        else:
+            url = f'http://{host}:{args.port}/'
+
+        # Launch the web browser on a thread as webbrowser.open may block
+        webbrowser_thread = threading.Thread(target=webbrowser.open, args=(url,))
+        webbrowser_thread.daemon = True
+        webbrowser_thread.start()
 
     # Host the application
-    GunicornServer.make_server(MarkdownUpApplication(root), host, args.port, when_ready)
+    StandaloneApplication(MarkdownUpApplication(root), {
+        'access_log_format': '%(h)s %(l)s "%(r)s" %(s)s %(b)s',
+        'accesslog': '-',
+        'errorlog': '-',
+        'bind': f'{host}:{args.port}',
+        'when_ready': when_ready
+    }).run()
 
 
-# A stand-alone WSGI application using Gunicorn
-class GunicornServer(gunicorn.app.base.BaseApplication):
+# A stand-alone WSGI server using Gunicorn - see https://docs.gunicorn.org/en/stable/custom.html
+class StandaloneApplication(gunicorn.app.base.BaseApplication):
     # pylint: disable=abstract-method
 
-    def __init__(self, app, host, port, when_ready):
-        self.options = {
-            'access_log_format': '%(h)s %(l)s "%(r)s" %(s)s %(b)s',
-            'accesslog': '-',
-            'errorlog': '-',
-            'bind': f'{host}:{port}'
-        }
-        if when_ready is not None:
-            self.options['when_ready'] = when_ready
+    def __init__(self, application, options): # pragma: no cover
+        self.options = options
+        self.application = application
         super().__init__()
-        self.callable = app
 
-    def load_config(self):
+    def load_config(self): # pragma: no cover
         for key, value in self.options.items():
             self.cfg.set(key, value)
 
-    @classmethod
-    def make_server(cls, app, host, port, when_ready = None):
-        cls(app, host, port, when_ready).run()
+    def load(self): # pragma: no cover
+        return self.application
--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -8,7 +8,7 @@ from unittest.mock import ANY, patch
 
 import markdown_up.__main__
 from markdown_up.app import MarkdownUpApplication
-from markdown_up.main import GunicornServer, main
+from markdown_up.main import main
 
 
 class TestMain(unittest.TestCase):
@@ -19,10 +19,15 @@ class TestMain(unittest.TestCase):
     def test_main_help(self):
         with patch('sys.stdout', StringIO()) as stdout, \
              patch('sys.stderr', StringIO()) as stderr, \
-             patch('os.path.isfile', False):
+             patch('os.path.isdir', return_value=False) as mock_isdir, \
+             patch('os.path.isfile', return_value=False) as mock_isfile:
             with self.assertRaises(SystemExit) as cm_exc:
                 main(['-h'])
 
+            self.assertNotEqual(stdout.getvalue(), '')
+            self.assertEqual(stderr.getvalue(), '')
+            mock_isdir.assert_not_called()
+            mock_isfile.assert_not_called()
             self.assertEqual(cm_exc.exception.code, 0)
             self.assertEqual(stdout.getvalue().splitlines()[0], 'usage: markdown-up [-h] [-p N] [-n] [path]')
             self.assertEqual(stderr.getvalue(), '')
@@ -30,10 +35,15 @@ class TestMain(unittest.TestCase):
     def test_main_file_not_found(self):
         with patch('sys.stdout', StringIO()) as stdout, \
              patch('sys.stderr', StringIO()) as stderr, \
+             patch('os.path.isdir', return_value=False) as mock_isdir, \
              patch('os.path.isfile', return_value=False) as mock_isfile:
             with self.assertRaises(SystemExit) as cm_exc:
                 main([os.path.join('missing', 'README.md')])
 
+            self.assertEqual(stdout.getvalue(), '')
+            self.assertEqual(stderr.getvalue(), '"missing/README.md" does not exist!\n')
+            mock_isdir.assert_called_once_with('missing/README.md')
+            mock_isfile.assert_called_once_with('missing/README.md')
             self.assertEqual(cm_exc.exception.code, 2)
             self.assertEqual(stdout.getvalue(), '')
             self.assertTrue(stderr.getvalue(), '"missing/README.md" does not exist!\n')
@@ -42,10 +52,15 @@ class TestMain(unittest.TestCase):
     def test_main_dir_not_found(self):
         with patch('sys.stdout', StringIO()) as stdout, \
              patch('sys.stderr', StringIO()) as stderr, \
-             patch('os.path.isdir', return_value=False) as mock_isdir:
+             patch('os.path.isdir', return_value=False) as mock_isdir, \
+             patch('os.path.isfile', return_value=False) as mock_isfile:
             with self.assertRaises(SystemExit) as cm_exc:
                 main(['missing'])
 
+            self.assertEqual(stdout.getvalue(), '')
+            self.assertEqual(stderr.getvalue(), '"missing" does not exist!\n')
+            mock_isdir.assert_called_once_with('missing')
+            mock_isfile.assert_called_once_with('missing')
             self.assertEqual(cm_exc.exception.code, 2)
             self.assertEqual(stdout.getvalue(), '')
             self.assertEqual(stderr.getvalue(), '"missing" does not exist!\n')
@@ -54,19 +69,28 @@ class TestMain(unittest.TestCase):
     def test_main_run(self):
         with patch('sys.stdout', StringIO()) as stdout, \
              patch('sys.stderr', StringIO()) as stderr, \
-             patch('os.path.isdir', return_value=True) as mock_isfile, \
+             patch('os.path.isdir', return_value=True) as mock_isdir, \
+             patch('os.path.isfile', return_value=False) as mock_isfile, \
              patch('threading.Thread') as mock_thread, \
-             patch('markdown_up.main.GunicornServer.make_server') as mock_make_server:
+             patch('markdown_up.main.StandaloneApplication') as mock_server:
             main([])
 
             self.assertEqual(stdout.getvalue(), '')
             self.assertEqual(stderr.getvalue(), '')
-            mock_isfile.assert_called_once_with('.')
+            mock_isdir.assert_called_once_with('.')
+            mock_isfile.assert_not_called()
             mock_thread.assert_not_called()
-            mock_make_server.assert_called_once_with(ANY, '127.0.0.1', 8080, ANY)
-            self.assertIsInstance(mock_make_server.mock_calls[0].args[0], MarkdownUpApplication)
-            self.assertEqual(mock_make_server.mock_calls[0].args[0].root, '.')
-            when_ready = mock_make_server.mock_calls[0].args[3]
+            mock_server.assert_called_once_with(ANY, {
+                'access_log_format': '%(h)s %(l)s "%(r)s" %(s)s %(b)s',
+                'accesslog': '-',
+                'errorlog': '-',
+                'bind': '127.0.0.1:8080',
+                'when_ready': ANY
+            })
+            mock_server.return_value.run.assert_called_once_with()
+            self.assertIsInstance(mock_server.mock_calls[0].args[0], MarkdownUpApplication)
+            self.assertEqual(mock_server.mock_calls[0].args[0].root, '.')
+            when_ready = mock_server.mock_calls[0].args[1]['when_ready']
             self.assertTrue(callable(when_ready))
             when_ready(None)
             mock_thread.assert_called_once_with(target=ANY, args=('http://127.0.0.1:8080/',))
@@ -75,19 +99,28 @@ class TestMain(unittest.TestCase):
     def test_main_run_port(self):
         with patch('sys.stdout', StringIO()) as stdout, \
              patch('sys.stderr', StringIO()) as stderr, \
-             patch('os.path.isdir', return_value=True) as mock_isfile, \
+             patch('os.path.isdir', return_value=True) as mock_isdir, \
+             patch('os.path.isfile', return_value=False) as mock_isfile, \
              patch('threading.Thread') as mock_thread, \
-             patch('markdown_up.main.GunicornServer.make_server') as mock_make_server:
+             patch('markdown_up.main.StandaloneApplication') as mock_server:
             main(['-p', '8081'])
 
             self.assertEqual(stdout.getvalue(), '')
             self.assertEqual(stderr.getvalue(), '')
-            mock_isfile.assert_called_once_with('.')
+            mock_isdir.assert_called_once_with('.')
+            mock_isfile.assert_not_called()
             mock_thread.assert_not_called()
-            mock_make_server.assert_called_once_with(ANY, '127.0.0.1', 8081, ANY)
-            self.assertIsInstance(mock_make_server.mock_calls[0].args[0], MarkdownUpApplication)
-            self.assertEqual(mock_make_server.mock_calls[0].args[0].root, '.')
-            when_ready = mock_make_server.mock_calls[0].args[3]
+            mock_server.assert_called_once_with(ANY, {
+                'access_log_format': '%(h)s %(l)s "%(r)s" %(s)s %(b)s',
+                'accesslog': '-',
+                'errorlog': '-',
+                'bind': '127.0.0.1:8081',
+                'when_ready': ANY
+            })
+            mock_server.return_value.run.assert_called_once_with()
+            self.assertIsInstance(mock_server.mock_calls[0].args[0], MarkdownUpApplication)
+            self.assertEqual(mock_server.mock_calls[0].args[0].root, '.')
+            when_ready = mock_server.mock_calls[0].args[1]['when_ready']
             self.assertTrue(callable(when_ready))
             when_ready(None)
             mock_thread.assert_called_once_with(target=ANY, args=('http://127.0.0.1:8081/',))
@@ -96,19 +129,28 @@ class TestMain(unittest.TestCase):
     def test_main_run_file(self):
         with patch('sys.stdout', StringIO()) as stdout, \
              patch('sys.stderr', StringIO()) as stderr, \
+             patch('os.path.isdir', return_value=False) as mock_isdir, \
              patch('os.path.isfile', return_value=True) as mock_isfile, \
              patch('threading.Thread') as mock_thread, \
-             patch('markdown_up.main.GunicornServer.make_server') as mock_make_server:
+             patch('markdown_up.main.StandaloneApplication') as mock_server:
             main(['README.md'])
 
             self.assertEqual(stdout.getvalue(), '')
             self.assertEqual(stderr.getvalue(), '')
+            mock_isdir.assert_called_once_with('README.md')
             mock_isfile.assert_called_once_with('README.md')
             mock_thread.assert_not_called()
-            mock_make_server.assert_called_once_with(ANY, '127.0.0.1', 8080, ANY)
-            self.assertIsInstance(mock_make_server.mock_calls[0].args[0], MarkdownUpApplication)
-            self.assertEqual(mock_make_server.mock_calls[0].args[0].root, '.')
-            when_ready = mock_make_server.mock_calls[0].args[3]
+            mock_server.assert_called_once_with(ANY, {
+                'access_log_format': '%(h)s %(l)s "%(r)s" %(s)s %(b)s',
+                'accesslog': '-',
+                'errorlog': '-',
+                'bind': '127.0.0.1:8080',
+                'when_ready': ANY
+            })
+            mock_server.return_value.run.assert_called_once_with()
+            self.assertIsInstance(mock_server.mock_calls[0].args[0], MarkdownUpApplication)
+            self.assertEqual(mock_server.mock_calls[0].args[0].root, '.')
+            when_ready = mock_server.mock_calls[0].args[1]['when_ready']
             self.assertTrue(callable(when_ready))
             when_ready(None)
             mock_thread.return_value.start.assert_called_once()
@@ -118,20 +160,28 @@ class TestMain(unittest.TestCase):
     def test_main_run_file_subdir(self):
         with patch('sys.stdout', StringIO()) as stdout, \
              patch('sys.stderr', StringIO()) as stderr, \
+             patch('os.path.isdir', return_value=False) as mock_isdir, \
              patch('os.path.isfile', return_value=True) as mock_isfile, \
              patch('threading.Thread') as mock_thread, \
-             patch('markdown_up.main.GunicornServer.make_server') as mock_make_server:
+             patch('markdown_up.main.StandaloneApplication') as mock_server:
             main(['subdir/README.md'])
 
             self.assertEqual(stdout.getvalue(), '')
             self.assertEqual(stderr.getvalue(), '')
+            mock_isdir.assert_called_once_with('subdir/README.md')
             mock_isfile.assert_called_once_with('subdir/README.md')
             mock_thread.assert_not_called()
-            mock_make_server.assert_called_once_with(ANY, '127.0.0.1', 8080, ANY)
-            self.assertIsInstance(mock_make_server.mock_calls[0].args[0], MarkdownUpApplication)
-            self.assertEqual(mock_make_server.mock_calls[0].args[0].root, 'subdir')
-            self.assertTrue(callable(mock_make_server.mock_calls[0].args[3]))
-            when_ready = mock_make_server.mock_calls[0].args[3]
+            mock_server.assert_called_once_with(ANY, {
+                'access_log_format': '%(h)s %(l)s "%(r)s" %(s)s %(b)s',
+                'accesslog': '-',
+                'errorlog': '-',
+                'bind': '127.0.0.1:8080',
+                'when_ready': ANY
+            })
+            mock_server.return_value.run.assert_called_once_with()
+            self.assertIsInstance(mock_server.mock_calls[0].args[0], MarkdownUpApplication)
+            self.assertEqual(mock_server.mock_calls[0].args[0].root, 'subdir')
+            when_ready = mock_server.mock_calls[0].args[1]['when_ready']
             self.assertTrue(callable(when_ready))
             when_ready(None)
             mock_thread.assert_called_once_with(target=ANY, args=('http://127.0.0.1:8080/#url=README.md',))
@@ -140,63 +190,28 @@ class TestMain(unittest.TestCase):
     def test_main_run_no_browser(self):
         with patch('sys.stdout', StringIO()) as stdout, \
              patch('sys.stderr', StringIO()) as stderr, \
-             patch('os.path.isdir', return_value=True) as mock_isfile, \
+             patch('os.path.isdir', return_value=True) as mock_isdir, \
+             patch('os.path.isfile', return_value=False) as mock_isfile, \
              patch('threading.Thread') as mock_thread, \
-             patch('markdown_up.main.GunicornServer.make_server') as mock_make_server:
+             patch('markdown_up.main.StandaloneApplication') as mock_server:
             main(['-n'])
 
             self.assertEqual(stdout.getvalue(), '')
             self.assertEqual(stderr.getvalue(), '')
-            mock_isfile.assert_called_once_with('.')
+            mock_isdir.assert_called_once_with('.')
+            mock_isfile.assert_not_called()
             mock_thread.assert_not_called()
-            mock_make_server.assert_called_once_with(ANY, '127.0.0.1', 8080, None)
-            self.assertIsInstance(mock_make_server.mock_calls[0].args[0], MarkdownUpApplication)
-            self.assertEqual(mock_make_server.mock_calls[0].args[0].root, '.')
-
-    def test_make_server(self):
-        class TestServer(GunicornServer):
-            # pylint: disable=abstract-method
-            def run(self):
-                testcase.assertIsInstance(self, GunicornServer)
-                testcase.assertIs(self.callable, app)
-                testcase.assertDictEqual(self.options, {
-                    'access_log_format': '%(h)s %(l)s "%(r)s" %(s)s %(b)s',
-                    'accesslog': '-',
-                    'errorlog': '-',
-                    'bind': '127.0.0.1:8080'
-                })
-                testcase.assertEqual(self.cfg.access_log_format, '%(h)s %(l)s "%(r)s" %(s)s %(b)s')
-                testcase.assertEqual(self.cfg.accesslog, '-')
-                testcase.assertEqual(self.cfg.errorlog, '-')
-                testcase.assertListEqual(self.cfg.bind, ['127.0.0.1:8080'])
-
-        testcase = self
-        app = MarkdownUpApplication('.')
-        TestServer.make_server(app, '127.0.0.1', 8080)
-
-    def test_make_server_when_ready(self):
-        class TestServer(GunicornServer):
-            # pylint: disable=abstract-method
-            def run(self):
-                testcase.assertIsInstance(self, GunicornServer)
-                testcase.assertIs(self.callable, app)
-                testcase.assertDictEqual(self.options, {
-                    'access_log_format': '%(h)s %(l)s "%(r)s" %(s)s %(b)s',
-                    'accesslog': '-',
-                    'errorlog': '-',
-                    'bind': '127.0.0.1:8080',
-                    'when_ready': when_ready
-                })
-                testcase.assertEqual(self.cfg.access_log_format, '%(h)s %(l)s "%(r)s" %(s)s %(b)s')
-                testcase.assertEqual(self.cfg.accesslog, '-')
-                testcase.assertEqual(self.cfg.errorlog, '-')
-                testcase.assertListEqual(self.cfg.bind, ['127.0.0.1:8080'])
-                testcase.assertIs(self.cfg.when_ready, when_ready)
-
-        def when_ready(_):
-            pass
-
-        testcase = self
-        app = MarkdownUpApplication('.')
-        TestServer.make_server(app, '127.0.0.1', 8080, when_ready)
-        when_ready(None)
+            mock_server.assert_called_once_with(ANY, {
+                'access_log_format': '%(h)s %(l)s "%(r)s" %(s)s %(b)s',
+                'accesslog': '-',
+                'errorlog': '-',
+                'bind': '127.0.0.1:8080',
+                'when_ready': ANY
+            })
+            mock_server.return_value.run.assert_called_once_with()
+            self.assertIsInstance(mock_server.mock_calls[0].args[0], MarkdownUpApplication)
+            self.assertEqual(mock_server.mock_calls[0].args[0].root, '.')
+            when_ready = mock_server.mock_calls[0].args[1]['when_ready']
+            self.assertTrue(callable(when_ready))
+            when_ready(None)
+            mock_thread.assert_not_called()
