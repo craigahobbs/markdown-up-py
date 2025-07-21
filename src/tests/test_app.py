327d6fc831db354d2309d5db1e24bc0df56a3a35
Craig A. Hobbs <craigahobbs@gmail.com>
2025-07-21 16:12:55 -0700

    add wsgi api test

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -406,6 +406,44 @@ endfunction
             self.assertDictEqual(json.loads(content_bytes.decode('utf-8')), {'result': 3.5})
 
 
+    def test_api_wsgi(self):
+        test_files = [
+            ('test.smd', '''\
+action sumNumbers
+    urls
+        GET
+
+    query
+        float[] values
+'''),
+            ('test.bare', '''\
+function sumNumbers(request):
+    result = 0
+    for value in objectGet(request, 'values'):
+        result = result + value
+    endfor
+    return arrayNew( \
+        '200 OK', \
+        arrayNew(arrayNew('Content-Type', 'text/plain')), \
+        'The result is ' + result \
+    )
+endfunction
+''')
+        ]
+        with create_test_files(test_files) as temp_dir:
+            app = MarkdownUpApplication(temp_dir, {}, {
+                'schemas': ['test.smd'],
+                'scripts': ['test.bare'],
+                'apis': [
+                    {'name': 'sumNumbers', 'wsgi': True}
+                ]
+            })
+            status, headers, content_bytes = app.request('GET', '/sumNumbers', query_string='values.0=1&values.1=2.5')
+            self.assertEqual(status, '200 OK')
+            self.assertEqual(headers, [('Content-Type', 'text/plain')])
+            self.assertEqual(content_bytes, b'The result is 3.5')
+
+
 class TestMarkdownUpAPI(unittest.TestCase):
 
     def test_markdown_up_index(self):
